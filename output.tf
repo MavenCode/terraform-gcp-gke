ec439dfdc7ffcb54a1bbdd5a6735a7061afdbec8
charles adetiloye <charlesa101@users.noreply.github.com>
2022-01-04 17:45:58 -0600

    Update output.tf

--- output.tf
+++ output.tf
@@ -2,14 +2,6 @@ output "gke_host" {
   value = "https://${google_container_cluster.main_gke_cluster.endpoint}"
 }
 
-output "gke_username" {
-  value = google_container_cluster.main_gke_cluster.master_auth[0].username
-}
-
-output "gke_password" {
-  value = google_container_cluster.main_gke_cluster.master_auth[0].password
-}
-
 output "gke_client_certificate" {
   value = base64decode(google_container_cluster.main_gke_cluster.master_auth[0].client_certificate)
 }
