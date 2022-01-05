4a16f7a9dae6eb06b2f9193da27acfa3debbaaec
charles adetiloye <charlesa101@users.noreply.github.com>
2022-01-04 18:30:40 -0600

    Update output.tf

--- output.tf
+++ output.tf
@@ -3,7 +3,7 @@ output "gke_host" {
 }
 
 output "gke_client_certificate" {
-  value = base64decode(google_container_cluster.main_gke_cluster.master_auth[0].client_certificate)
+  value = base64decode(google_container_cluster.main_gke_cluster.master_auth.0.cluster_ca_certificate)
 }
 
 output "gke_client_key" {
